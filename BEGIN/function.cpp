9846c4b04091fe1cfbbb05eb0e90e17d834656e7
AKACHI-4 <Adarshrawat8304@gmail.com>
2022-09-17 07:24:44 +0530

    new function file added to understand global variable range

---
+++ BEGIN/function.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int a = 3; 
+
+void fun(){
+    cout<<a<<endl;
+}
+
+signed main(){
+    
+    cout<<a<<endl;
+
+    a = 4; 
+    cout<<a<<endl;
+
+    fun(); 
+
+    cout<<a<<endl;
+
+    return 0;
+}
